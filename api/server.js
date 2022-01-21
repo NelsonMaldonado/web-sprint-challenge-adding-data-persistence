ba83563b0c37dcfb2f3778c24aa68cea3d31e81b
Nelson Maldonado <play2nino@gmail.com>
2022-01-21 13:23:09 -0800

    index

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+const express =  require('express')
+const server = express()
+
+server.get('/',(req,res)=>{
+res.status(200).send('first endpoint working')
+})
+
+module.exports =  server
