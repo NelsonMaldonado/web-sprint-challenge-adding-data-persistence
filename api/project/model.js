f5fce4ecebae26d4a5e0ecb6040682f0668a055b
Nelson Maldonado <play2nino@gmail.com>
2022-01-21 15:44:53 -0800

    knex is workingggggggggggg finally

--- api/project/model.js
+++ api/project/model.js
@@ -7,3 +7,20 @@ const convertObject = (project) => {
     project_completed: !!project.project_completed,
   }
 }
+
+const getAll =()=>{
+    db('projects').then(projects=>{
+        projects.map(convertObject)
+    })
+}
+
+const insert = async (project)=>{
+let project_id;
+await db("projects").insert(project).then(([id])=>project_id)
+return db('projects').where({project_id}).first().then(convertObject)
+}
+
+module.exports = {
+    getAll,
+    insert
+}
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const server = express()
 const routerProject = require("./project/router")
+
 server.use(express.json())
 server.use("/api/projects", routerProject)
 
