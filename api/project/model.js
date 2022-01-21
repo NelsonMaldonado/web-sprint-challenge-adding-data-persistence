db40b568c1ae338bac8a402efc13abdcf3466848
Nelson Maldonado <play2nino@gmail.com>
2022-01-21 14:07:21 -0800

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,9 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig")
+
+const convertObject = (project) => {
+  return {
+    ...project,
+    project_completed: !!project.project_completed,
+  }
+}
