acbca7afc9e0e7a0404b122f35a1f1d1b222172f
Nelson Maldonado <play2nino@gmail.com>
2022-01-21 16:35:53 -0800

    wip router resource

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,18 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig")
+const getAll = () => {
+  db("resources")
+}
+
+const insert = () => {
+  let resource_id
+  await db("resources")
+    .insert(resource)
+    .then((id) => (resource_id = id))
+  return db("resources").where({ resource_id }).first()
+}
+
+module.exports = {
+  getAll,
+  insert,
+}
